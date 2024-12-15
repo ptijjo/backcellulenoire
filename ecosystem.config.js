32f416edeb5594d8f45de813d714e1ce15288f42
Joseph Bangoura <ptijjo@hotmail.com>
2024-12-15 15:01:10 +0100

    refaire le port dans ecosysteme.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
       env: { // environment variable
-        PORT: 3000,
+        PORT: 8080,
         NODE_ENV: 'production',
       },
     },
@@ -39,7 +39,7 @@
       output: './logs/access.log', // pm2 log file
       error: './logs/error.log', // pm2 error log file
       env: { // environment variable
-        PORT: 3000,
+        PORT: 8080,
         NODE_ENV: 'development',
       },
     },
